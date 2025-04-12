c23cae996ed87e3e9de4ff74bec7cdaab8c772f1
Daniil <tsarckov.backup@gmail.com>
2025-04-13 00:10:32 +0300

    clear upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from camera_scout import CameraResearcher
+
+def main():
+    Resercher = CameraResearcher(visualize=False)
+
+    if cam := Resercher.get_camera('cam'):
+        print(f"Using camera: {cam['name']}")
+        print(f"Resolution: {cam['cam_param']['width']}x{cam['cam_param']['height']}")
+
+    if cam := Resercher.get_camera('termal'):
+        print(f"Using camera: {cam['name']}")
+        print(f"Resolution: {cam['cam_param']['width']}x{cam['cam_param']['height']}")
+
+
+if __name__ == "__main__":
+    main()
